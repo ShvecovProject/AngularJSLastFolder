de1d8f1017e29edef6f8096109a5550ad2db0b0a
Evgeniy Shvecov <evgeshvecov@yandex.ru>
2016-03-10 19:54:46 +0200

    http

---
+++ http/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Shvecov_Evgeniy on 10.03.2016.
+ */
+angular.module("app",[])
+.controller("UserCtrl",function($scope, $http){
+
+    $scope.addUserToStore = function(){
+        var model = {
+            userName:$scope.userName,
+            userEmail :$scope.userEmail
+        };
+        $http.post('https://api.mlab.com/api/1/databases/scrumusers/collections/users/',
+            model,
+            {
+                params: {
+                    apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
+                }
+            })
+            .success(function(data, status, headers, config){
+                $scope.userName ="";
+                $scope.userEmail = "";
+                $scope.users.push(model);
+                console.log(data);
+            })
+            .error(function(data, status, headers, config){
+
+            });
+    };
+
+        $scope.getAllUsers=function(){
+            $http.get('https://api.mlab.com/api/1/databases/scrumusers/collections/users/',
+                {
+                    params: {
+                        apiKey:"H7NnGFZwutQwxFP6T8Kc_XhkQe4Z4ygX"
+                    }
+                })
+                .success(function(data, status, headers, config){
+                    $scope.users = data;
+                    console.log(data);
+                })
+                .error(function(data, status, headers, config){
+
+                });
+        }
+        $scope.users= $scope.getAllUsers();
+
+    });
